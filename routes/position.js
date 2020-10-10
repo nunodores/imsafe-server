8e3dac28952031f708856760f35ea00055e1fb52
Raphaël SEEGER <raphael.seeger@student.vinci.be>
2020-10-10 12:19:02 +0200

    solved post /pos/update

--- routes/position.js
+++ routes/position.js
@@ -4,24 +4,32 @@ const db = require('../modules/db');
 //const bcrypt = require('bcrypt');
 //var jwt = require('jsonwebtoken');
 
-/* GET users listing. */
+/* GET locations listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    db.db
+      .collection("Location")
+      .find()
+      .toArray()
+      .then(all_items => {
+        res.json(all_items);
+      })
+      .catch(err => {
+        console.log("Error within get alerts:", err);
+      });
 });
 
 router.post('/update', function(req, res) {
-    console.dir(req.body);
-
-   /*  db.db.collection('Location').replaceOne({uuid: req.body.uuid} ,{uuid: req.body.uuid, lat: req.body.lat,
+    db.db.collection('Location').replaceOne({uuid: req.body.uuid} ,{uuid: req.body.uuid, lat: req.body.lat,
         lon: req.body.lon}).then((result) => {
-        req.body._id = result.insertedId;
-        delete req.password;
-        res.json(req.body);
+            if(result) {
+                req.body._id = result.insertedId;
+                res.json(req.body);
+            } else {
+                res.status(500).send(err);
+            }
     }).catch((err) => {
         res.status(500).send(err);
-    }); */
-
-    res.json({"safe_level": 2});
+    }); 
 
 });
 
